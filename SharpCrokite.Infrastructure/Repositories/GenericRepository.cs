51429a1d610db7f1639c4f1101c844e72d3d9d5b
Michel Vlasblom <m.vlasblom@gmail.com>
2021-10-12 12:49:03 +0200

    Added GenericRepository. Added HarvestableRepository. Added MaterialRepository.

---
+++ SharpCrokite.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using SharpCrokite.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SharpCrokite.Infrastructure.Repositories
+{
+    public abstract class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected SharpCrokiteDbContext dbContext;
+
+        public GenericRepository(SharpCrokiteDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public virtual T Add(T entity)
+        {
+            return dbContext.Add(entity).Entity;
+        }
+
+        public virtual IEnumerable<T> All()
+        {
+            return dbContext.Set<T>().ToList();
+        }
+
+        public virtual IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return dbContext.Set<T>().AsQueryable().Where(predicate).ToList();
+        }
+
+        public virtual T Get(int id)
+        {
+            return dbContext.Find<T>(id);
+        }
+
+        public virtual void SaveChanges()
+        {
+            dbContext.SaveChanges();
+        }
+
+        public virtual T Update(T entity)
+        {
+            return dbContext.Update(entity).Entity;
+        }
+    }
+}
---
+++ SharpCrokite.Infrastructure/Repositories/MaterialRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharpCrokite.DataAccess;
+using SharpCrokite.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SharpCrokite.Infrastructure.Repositories
+{
+    public class MaterialRepository : GenericRepository<Material>
+    {
+        public MaterialRepository(SharpCrokiteDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public override Material Update(Material entity)
+        {
+            var material = dbContext.Materials
+                .Include(m => m.Prices)
+                .Single(m => m.MaterialId == entity.MaterialId);
+
+            material.Name = entity.Name;
+            material.Description = entity.Description;
+            material.Type = material.Type;
+            material.Icon = material.Icon;
+            material.Prices = material.Prices;
+
+            return base.Update(material);
+        }
+
+        public override IEnumerable<Material> Find(Expression<Func<Material, bool>> predicate)
+        {
+            return dbContext.Materials
+                .Include(m => m.Prices)
+                .Where(predicate)
+                .ToList();
+        }
+
+        public override IEnumerable<Material> All()
+        {
+            return dbContext.Materials
+                .Include(m => m.Prices)
+                .ToList();
+        }
+    }
+}
